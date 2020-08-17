18d104790fe763438794ab2f85253d493f20279d
jollyr0ger <kav993@gmail.com>
2020-08-17 14:48:26 +0300

    hide scrollbare during slide

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,7 @@ export default {
   bottom: 0;
   left: 0;
   right: 0;
-  overflow-y: scroll;
+  overflow-y: auto;
 }
 .relative-center{
   position: relative;
@@ -136,8 +136,31 @@ button:hover{
 }
 .slide-enter{
   transform: translateX(100vw);
+  overflow: hidden;
 }
 .slide-leave-to{
   transform: translateX(-100vw);
+  overflow: hidden;
+}
+
+/* Scrollbar */
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgb(135,206,235); 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(0, 169, 236); 
 }
 </style>
