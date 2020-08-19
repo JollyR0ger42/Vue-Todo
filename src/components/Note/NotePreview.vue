e65096992a9b6acf65a76956ae8f72d0f9f9d52e
jollyr0ger <kav993@gmail.com>
2020-08-19 16:08:59 +0300

    make smart NotePreview

--- src/components/Note/NotePreview.vue
+++ src/components/Note/NotePreview.vue
@@ -3,7 +3,7 @@
     <h1>{{note.title}}</h1>
     <hr>
     <ul>
-      <li class="todo" v-for="todo in note.todoList" :key="todo.id" :todo="todo">
+      <li class="todo" v-for="todo in previewTodo" :key="todo.id">
         {{todo.text}}
       </li>
     </ul>
@@ -19,7 +19,17 @@ import NoteDeleteButton from './NoteDeleteButton';
 
 export default {
   props: ['note'],
-  components: {NoteDeleteButton}
+  components: {NoteDeleteButton},
+  computed:{
+    previewTodo(){
+      let result = this.note.todoList.filter(todo => todo.status === false);
+      if(result.length > 5){
+        result = result.slice(0, 4)
+        result.push({id: 'dots', text: '...'})
+      }
+      return result
+    }
+  }
 }
 </script>
 
